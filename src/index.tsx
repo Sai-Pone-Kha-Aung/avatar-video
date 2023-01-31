d3a7a236e4adff1f70be3d1e06189840a6e44c86
Sai Pone Kha Aung <104347488+Sai-Pone-Kha-Aung@users.noreply.github.com>
2023-01-31 18:15:33 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GltfLoad from './gltf-load';
-import Avatar from './test';
-import TestOne from './testone';
 import ThreeScene from './three-scene';
 
 
